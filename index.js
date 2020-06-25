05086fd863b41b5e4ddb29ea9907439accfb2bca
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-25 21:41:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(array, func) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(func(array[i]));
+  }
+  return newArray;
+}
+
+function reduce(array, callback, start = 0) {
+  let r = !!start ? start : array[0];
+  let i = !!start ? 0 : 1;
+  for ( ; i < array.length; i++) {
+    r = callback(array[i], r);
+  }
+  return r;
+}
